7a185070a622678262d7e9e3fd5b05c2157dc4c8
dpdesai02 <dpdesai2884@gmail.com>
2023-04-13 22:47:41 +0100

    java-week-5-homework-dhara

---
+++ src/Programme_4_ColoursArrayList.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+/*
+Write a Java program to create a new array list, add some colours (string)
+and printout the collection using for each loop.
+ */
+
+
+
+    public class Programme_4_ColoursArrayList {
+        public static void main(String[] args) {
+            ArrayList<String> colorList = new ArrayList<>();
+        colorList.add("Red");
+        colorList.add("Blue");
+        colorList.add("Green");
+        colorList.add("Yellow");
+        colorList.add("Black");
+        colorList.add("White");
+        colorList.add("Orange");
+        colorList.add("Purple");
+        colorList.add("Clan");
+
+        for (String colourList : colorList) System.out.print(colourList + ", ");
+        }
+
+    }
+
